1986c078f0121a0159aeb369c7847a0446851855
cevahirakar27 <cevahirakar27@gmail.com>
2023-05-26 19:54:17 +0300

    26.05.23

--- src/main/java/day11_whileloop/C01_WhileLoop.java
+++ src/main/java/day11_whileloop/C01_WhileLoop.java
@@ -6,7 +6,7 @@ public class C01_WhileLoop {
     public static void main(String[] args) {
 
            /*
-            Kullanicidan bir tamsayi aliniz
+            Kullanicidan bir tamsayi aliniz.
             Tamsayi 100'den kucuk ise kullaniciya "Kaybettiniz!" mesaji vererek oyunu sonlandiriniz
             Tamsayi 100 ve 100'den buyuk ise "Kazandiniz!" mesaji vererek oyuna devam ettiriniz
          */
